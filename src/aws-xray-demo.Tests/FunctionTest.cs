de117352a771dbba76abb4ae66ea5fe5e0526188
sharath <sharathgopinathrao@gmail.com>
2021-02-23 23:25:50 +1100

    Function that takes a string and does a ToUpper and saves in database

--- src/aws-xray-demo.Tests/FunctionTest.cs
+++ src/aws-xray-demo.Tests/FunctionTest.cs
@@ -7,22 +7,12 @@ using Xunit;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
 
-using aws_xray_demo;
+using aws_xray_demo.Trigger;
 
-namespace aws_xray_demo.Tests
+namespace aws_xray_demo.Trigger.Tests
 {
     public class FunctionTest
     {
-        [Fact]
-        public void TestToUpperFunction()
-        {
-
-            // Invoke the lambda function and confirm the string was upper cased.
-            var function = new Function();
-            var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
-
-            Assert.Equal("HELLO WORLD", upperCase);
-        }
+        //TODO
     }
 }
